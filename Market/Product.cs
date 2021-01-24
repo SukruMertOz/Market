ac6acf2f744f73f904ae9bad300b930a7ccb9d67
SukruMertOz <77850643+SukruMertOz@users.noreply.github.com>
2021-01-24 17:51:31 +0300

    Add project files.

---
+++ Market/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Market
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string explanation { get; set; }
+        public int stock { get; set; }
+
+
+
+    }
+}
---
+++ Market/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "Orange";
+            product1.Price = 14;
+            product1.stock = 300;
+            product1.explanation = "Fresh Orange";
+
+
+            Product product2 = new Product();
+            product2.Name = "Berry";
+            product2.Price = 30;
+            product2.stock = 150;
+            product2.explanation = "Frozed Berry";
+
+            Product product3 = new Product(); 
+            product3.Name = "Apple";
+            product3.Price = 5;
+            product3.stock = 3000;
+            product3.explanation = "Imported Apple";
+
+            Product[] products = new Product[] { product1, product2, product3 };
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.stock);
+                Console.WriteLine(product.explanation);
+            }
+               
+        }
+    }
+}
